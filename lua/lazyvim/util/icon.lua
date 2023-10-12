79945a0fd9f8487ce3fbcb9d6b883ab362183930
jo-project <jo.project.0911@gmail.com>
2023-10-12 23:58:48 +1100

    LazyVim

--- lua/lazyvim/util/icon.lua
+++ lua/lazyvim/util/icon.lua
@@ -7,8 +7,8 @@ function M.get_icon(kind, padding, no_fallback)
     if not vim.g.icons_enabled and no_fallback then return "" end
     local icon_pack = vim.g.icons_enabled and "icons" or "text_icons"
     if not M[icon_pack] then
-        M.icons = require "icons.nerd_font"
-        M.text_icons = require "icons.text"
+        M.icons = require "lazyvim.icons.nerd_font"
+        M.text_icons = require "lazyvim.icons.text"
     end
     local icon = M[icon_pack] and M[icon_pack][kind]
     return icon and icon .. string.rep(" ", padding or 0) or ""
