24ba1aca666c8dd7b6f630682098dd90cb67b55a
jo-project <jo.project.0911@gmail.com>
2023-10-13 18:41:41 +1100

    LazyVim

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -280,6 +280,9 @@ return {
           ["vim.lsp.util.stylize_markdown"] = true,
           ["cmp.entry.get_documentation"] = true,
         },
+        signature = {
+          enabled = false
+        }
       },
       routes = {
         {
