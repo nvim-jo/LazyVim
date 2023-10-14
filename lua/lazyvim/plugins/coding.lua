137d796697378dcfd944403530d93ee0b6c6a86b
jo-project <jo.project.0911@gmail.com>
2023-10-15 03:51:16 +1100

    LazyVim 10.1.1

--- lua/lazyvim/plugins/coding.lua
+++ lua/lazyvim/plugins/coding.lua
@@ -58,7 +58,7 @@ return {
         window = {
           completion = cmp.config.window.bordered({
             border = "rounded",
-            winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None"
+            winhighlight = "Normal:Pmenu,FloatBorder:CmpBorder,CursorLine:PmenuSel,Search:None"
           }),
           documentation = cmp.config.window.bordered()
         },
