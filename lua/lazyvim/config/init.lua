86ed8d5e6e4df1abec24ab37977a8dcdcbf229f6
jo-project <jo.project.0911@gmail.com>
2023-10-16 09:11:12 +1100

    LazyVim 10.3.0

--- lua/lazyvim/config/init.lua
+++ lua/lazyvim/config/init.lua
@@ -22,7 +22,7 @@ local defaults = {
   news = {
     -- When enabled, NEWS.md will be shown when changed.
     -- This only contains big new features and breaking changes.
-    lazyvim = true,
+    lazyvim = false,
     -- Same but for Neovim's news.txt
     neovim = false,
   },
