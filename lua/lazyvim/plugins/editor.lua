c43b9d43ed1d586674f9a36eb5d9e290e3a35f2f
jo-project <jo.project.0911@gmail.com>
2023-10-22 22:48:06 +1100

    LazyVim 10.5.0

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -59,7 +59,7 @@ return {
       filesystem = {
         bind_to_cwd = true,
         follow_current_file = { enabled = true },
-        use_libuv_file_watcher = true,
+        use_libuv_file_watcher = false,
       },
       window = {
         mappings = {
