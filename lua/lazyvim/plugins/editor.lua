f381787e2658f9587d502a8d8c8e5d76a6d1b9d9
jo-project <jo.project.0911@gmail.com>
2023-11-20 09:12:08 +1100

    LazyVim 10.8.0

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -151,6 +151,14 @@ return {
             local padding = config.padding or " "
             local highlight = config.highlight or highlights.FILE_ICON
 
+            if node:get_depth() == 1 then
+              highlight = highlights.DIRECTORY_ICON
+              return {
+                text = "",
+                highlight = highlight
+              }
+            end
+
             if node.type == "directory" then
               highlight = highlights.DIRECTORY_ICON
               if node:is_expanded() then
