1552627f85d30b3a16dca71e64ec1af9becce60f
jo-project <jo.project.0911@gmail.com>
2023-11-20 02:15:59 +1100

    LazyVim 10.8.0

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -272,7 +272,7 @@ return {
   -- the highlighting.
   {
     "echasnovski/mini.indentscope",
-    enabled = false,
+    -- enabled = false,
     version = false, -- wait till new 0.7.0 release to put it back on semver
     event = "LazyFile",
     opts = {
