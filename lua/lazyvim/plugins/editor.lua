ff3fe2979016b3501a4dafadb5eaa0b39324b2f4
jo-project <jo.project.0911@gmail.com>
2023-11-20 07:42:45 +1100

    LazyVim 10.8.0

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -81,6 +81,35 @@ return {
         follow_current_file = { enabled = true },
         use_libuv_file_watcher = true,
         hijack_netrw_behavior = "disabled",
+        components = {
+          icon = function(config, node, state)
+            local highlights = require("neo-tree.ui.highlights")
+            local icon = config.default or " "
+            local padding = config.padding or " "
+            local highlight = config.highlight or highlights.FILE_ICON
+
+            if node.type == "directory" then
+              highlight = highlights.DIRECTORY_ICON
+              if node:is_expanded() then
+                icon = config.folder_open or "-"
+              else
+                icon = config.folder_closed or "+"
+              end
+            elseif node.type == "file" then
+              local success, web_devicons = pcall(require, "nvim-web-devicons")
+              if success then
+                local devicon, hl = web_devicons.get_icon(node.name, node.ext)
+                icon = devicon or icon
+                highlight = hl or highlight
+              end
+            end
+
+            return {
+              text = icon .. padding,
+              highlight = highlight,
+            }
+          end,
+        },
       },
       window = {
         mappings = {
